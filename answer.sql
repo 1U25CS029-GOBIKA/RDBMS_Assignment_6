f5846cc44ba27b852156437f2471f2f9dadf708a
prabhak-rvs <prabhak@rvsgroup.com>
2026-08-21 15:19:04 +0530

    Update and delete student records in answer.sql

--- answer.sql
+++ answer.sql
@@ -1,13 +1,10 @@
 USE CollegeDB;
 
--- Update Karthik's department
 UPDATE Student
 SET DepartmentID = 103
 WHERE StudentID = 1003;
 
--- Delete student with StudentID 1002
 DELETE FROM Student
 WHERE StudentID = 1002;
 
--- Display all student records
 SELECT * FROM Student;
